4300423fa13b38286bb754167ee1ed988eaf7fea
Jorge Rivera <jariverai@gmail.com>
2019-12-12 21:32:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
 population = pd.read_csv("poblacion.csv")
 print(population)
 
-stat = pd.DataFrame()
-stat["mean"] = population.groupby("Code")["Total"].mean()
-print(stat)
+stats = pd.DataFrame()
+stats["avg"] = population.groupby("Code")["Total"].mean()
+print(stats)
 
-stat["devstd"] = population.groupby("Code")["Total"].std()
-print(stat)
+stats["devstd"] = population.groupby("Code")["Total"].std()
+print(stats)
+
+stats = stats.sort_values(by = "avg", ascending = False)
+print(stats)
+
+topAvgs = pd.DataFrame()
+topAvgs = stats[stats["avg"] > 200000000]
+print(topAvgs)
+
+x = range(0, len(topAvgs.index))
+print(x)
+
+plt.plot(topAvgs.index, topAvgs["avg"], color = "green", marker = "o", label = "Promedio de la poblacion")
+plt.plot(topAvgs.index, topAvgs["devstd"], color = "blue", marker = "+", label = "Desviacion estandar")
+
+plt.legend()
+plt.xticks(rotation = 90)
+plt.xlabel("Pais")
+plt.ylabel("Poblacion")
+plt.grid(True)
+plt.title("Analisis de la poblacion mundial")
+plt.show()
