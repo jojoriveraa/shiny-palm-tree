5f774f7ffadccc718635b98b7fc2fd7edd758dbb
Jorge Rivera <jariverai@gmail.com>
2019-12-12 21:46:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,18 @@ print(topAvgs)
 x = range(0, len(topAvgs.index))
 print(x)
 
-plt.plot(topAvgs.index, topAvgs["avg"], color = "green", marker = "o", label = "Promedio de la poblacion")
-plt.plot(topAvgs.index, topAvgs["devstd"], color = "blue", marker = "+", label = "Desviacion estandar")
+plt.scatter (x, topAvgs["avg"], color = "green", marker = "o", label = "Promedio de la poblacion")
+plt.plot(x, topAvgs["devstd"], color = "blue", marker = "+", label = "Desviacion estandar")
 
 plt.legend()
-plt.xticks(rotation = 90)
+plt.xticks(range(0,30,2))
+plt.yticks(range(0,6000000000,500000000))
+
+# plt.text(8, 3000000000, "Mi texto")
+# plt.xlim(0,50)
+# plt.ylin(0,700000000)
+
+
 plt.xlabel("Pais")
 plt.ylabel("Poblacion")
 plt.grid(True)
