45a775999da6baaad928adc652c2a40e5d1eba23
Jorge Rivera <jariverai@gmail.com>
2019-12-12 20:52:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+population = pd.read_csv("poblacion.csv")
+print(population)
+
+stat = pd.DataFrame()
+stat["mean"] = population.groupby("Code")["Total"].mean()
+print(stat)
+
+stat["devstd"] = population.groupby("Code")["Total"].std()
+print(stat)
